91316b2115e4a27deaa6d6d7851ffb20c5e98c7c
SaurabhNarwariya123 <139843132+SaurabhNarwariya123@users.noreply.github.com>
2024-11-28 22:40:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['lucide-react']
-    }
-  }
-};
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
